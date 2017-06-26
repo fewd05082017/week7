ba8cc8807a629e720364db7bba6f103da96b1d5a
luisabaeta <luisabaeta@gmail.com>
2017-06-26 18:40:30 -0400

    Draft 2
    So, I managed to fix one thing about the sheep jump I was trying to
    accomplish (there was a comma missing, and I moved the var inside the
    function). Now it moves but it still doesn’t do what I want it to do.
    But still. Yay?

--- js/project.js
+++ js/project.js
@@ -2,33 +2,35 @@
 // Lazy "Cow On Click" Animation
 // -------------------------
 
-$("body").on("click", function(evt){
-  var cow2 = $("<img/>");
-  cow2.attr("src", "images/cow2.gif");
-  cow2.css("left", evt.pageX);
-  cow2.css("top", evt.pageY);
-  $("body").append(cow2);
-});
+// $("body").on("click", function(evt){
+//   var cow2 = $("<img/>");
+//   cow2.attr("src", "images/cow2.gif");
+//   cow2.css("left", evt.pageX);
+//   cow2.css("top", evt.pageY);
+//   $("body").append(cow2);
+// });
 
 
 // ---------------------------
 // Ambitious (failed) attempt to make the sheep jump over the fence
 // ---------------------------
 
-// $("body").on("click", function(evt){
-// 	$("#sheep").animate({ left: "300px" }, 3000, "easeOutExpo");
+$("body").on("click", function(evt){
+	$("#sheep").animate({ left: "300px" }, 3000, "easeOutExpo");
 
-// 	var position
-// 	position = $("#sheep").css("left", "400px");
+	var jumpFence
+	jumpFence = $("#sheep").animate({ up: "2px", left: "2px" }, 3000);
 
-// 	if ($("#sheep").css("left", "760px"))   
-// 	sheepLeave
+	var sheepLeave
+	sheepLeave = $("#sheep").animate({ left: "2000px" }, 3000);
 
-// 	else jumpFence
-// });
+	var position
+	position = $("#sheep").css("left", "400px");
 
-// var jumpFence
-// jumpFence = $("#sheep").animate({ up: "2px" left: "2px" }, 3000);
+	if ($("#sheep").css("left", "760px"))   
+	sheepLeave
 
-// var sheepLeave
-// sheepLeave = $("#sheep").animate({ left: "2000px" }, 3000);
+	else jumpFence
+});
+
+
